0ce7b94d8109dfa8b394b9a59b022cf2840ab017
arbaevsherbolot <arbaevsherbolot1@gmail.com>
2024-03-21 23:15:22 +0600

    update

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -72,7 +72,7 @@ export class AuthController {
     return response
       .status(HttpStatus.OK)
       .clearCookie('session')
-      .redirect(process.env.FRONTEND_BASE_URL);
+      .send('<script>window.close();</script>');
   }
 
   @Get('me')
